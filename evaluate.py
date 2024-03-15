14f86dbf60d66ccebd22ea94a0e09f0dd46b2918
zhang yuyang <zhangyuyang1211@gmail.com>
2024-03-15 17:37:19 +0800

    remove

--- evaluate.py
+++ evaluate.py
@@ -1,22 +1,9 @@
 from engine import evaluate
-from dataloader.dataloader import fetch_dataloader 
-from model.vit import vit 
+from dataloader.dataloader import fetch_dataloader
+from model.vit import vit
 
 from utils.misc import load_checkpoint
 
 
-
-
-if __name__ == '__main__':
-    test_dataloader = fetch_dataloader(['test'])['test']
-    model = vit()
-    model_dir = ""
-    
-    load_checkpoint(model, model_dir)
-    
-    test_metrics = evaluate(
-        model,
-        loss_fn=,
-        metrics=,
-        params=
-    )
+if __name__ == "__main__":
+    raise NotImplementedError
